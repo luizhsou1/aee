f4b9c9643e257f19fce206244601a1aab2d9c8a0
Luiz Henrique <luiz.henrique@zenvia.com>
2022-03-20 13:13:12 -0300

    ci: added configs of deploy

---
+++ app/src/views/PageNotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>404 Page Not Found :/</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({ name: 'PageNotFound' })
+</script>
