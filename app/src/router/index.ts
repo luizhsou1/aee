5ffa1e8f21acc5ce73dbd3b8e8de5bf8c1655a81
Luiz Henrique <luizhsou1@gmail.com>
2022-04-04 07:59:18 -0300

    refactor: new frontend

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,25 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
-import PageNotFound from '../views/PageNotFound.vue'
+import LoginView from '../views/LoginView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFound',
-    component: PageNotFound
+    name: 'login',
+    component: LoginView
   }
 ]
 
